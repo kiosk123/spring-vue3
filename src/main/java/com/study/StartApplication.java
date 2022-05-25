5dda652390ed1db8f35e20c4c734e8fc2710fe73
HeoJongTae <jt0907@solulink.co.kr>
2022-05-25 11:16:37 +0900

    .

---
+++ src/main/java/com/study/StartApplication.java
@@ -0,0 +1,26 @@
+package com.study;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- insert into user(id, join_date, name, password, ssn) values(1, sysdate(), 'User1', 'test1111', '701010-1111111')
+-- insert into user(id, join_date, name, password, ssn) values(2, sysdate(), 'User2', 'test2222', '801010-2222222')
+-- insert into user(id, join_date, name, password, ssn) values(3, sysdate(), 'User3', 'test3333', '901010-1111111')
