3cc82367af3d2ef9276d0ccbf75e8994130135a7
HeoJongTae <jt0907@solulink.co.kr>
2022-05-25 13:18:05 +0900

    .

--- src/main/java/com/study/repository/MessageJdbcRepository.java
+++ src/main/java/com/study/repository/MessageJdbcRepository.java
@@ -1,10 +1,21 @@
 package com.study.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
 import javax.sql.DataSource;
 
-import org.springframework.stereotype.Repository;
+import com.study.vo.Message;
 
-@Repository
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MessageJdbcRepository {
     
     private final DataSource dataSource;
@@ -13,6 +24,40 @@ public class MessageJdbcRepository {
         this.dataSource = dataSource;
     }
 
-    
+    public Message saveMessage(Message message) {
+        Connection c = DataSourceUtils.getConnection(dataSource);
+        try {
+            String insertSql = "INSERT INTO messages ('id', 'text', 'created_date') VALUE (null, ?, ?)";
+            PreparedStatement ps = c.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
+            //SQL에 필요한 매개변수를 준비한다
+            ps.setString(1, message.getText());
+            ps.setTimestamp(2, new Timestamp(message.getCreatedDate().getTime()));
+            int rowsAffected = ps.executeUpdate();
 
+            if (rowsAffected > 0) {
+                // 새로 저장된 메시지 아이디 가져오기
+                ResultSet result = ps.getGeneratedKeys();
+                if (result.next()) {
+                    int id = result.getInt(1);
+                    return new Message(id, message.getText(), message.getCreatedDate());
+                } else {
+                    log.error("Failed to retrieve id. No row in result set");
+                    return null;
+                }
+            } else {
+                log.error("insert failed");
+                return null;
+            }
+        } catch (SQLException e) {
+            log.error("Failed to save message", e);
+            try {
+                if (c != null) c.close();
+            } catch (SQLException ex) {
+                log.error("Failed to close connection", e);
+            }
+        } finally {
+            DataSourceUtils.releaseConnection(c, dataSource);
+        }
+        return null;
+    }
 }
